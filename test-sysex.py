5690072a355074565a8346803a597279bd54004e
p3r7 <11557146+p3r7@users.noreply.github.com>
2021-04-20 17:19:32 +0200

    Control tree parsing

--- test-sysex.py
+++ test-sysex.py
@@ -1,6 +1,10 @@
+import math
+from functools import reduce # only in Python 3
+import operator
+from pprint import pprint
+
 import mido
 # import mido.backends.pygame
-from pprint import pprint
 
 
 
@@ -13,7 +17,17 @@ outport = mido.open_output('MidiSport 1x1 MIDI 1')
 
 
 
-## FNS
+## CORE: DICTS
+
+def getFromDict(dataDict, mapList):
+    return reduce(operator.getitem, mapList, dataDict)
+
+def setInDict(dataDict, mapList, value):
+    getFromDict(dataDict, mapList[:-1])[mapList[-1]] = value
+
+
+
+## CORE: BINARY ENCODING
 
 def neg_complement_maybe(v):
     if v<0:
@@ -27,11 +41,6 @@ def nibblize(v, size=2):
     while len(v_arr) < size:
         v_arr.append(0)
     return v_arr
-
-    # if len(v_str) == 1:
-    #     return [int(v_str[0], 16), 0]
-    # else:
-    #     return [int(v_str[1], 16), int(v_str[0], 16)]
 
 def unnibblize(v_arr):
     v_arr = [hex(v)[2:] for v in reversed(v_arr)]
@@ -48,6 +57,11 @@ def unnibblize_str(v_arr):
         out += chr(int(v, 16))
     return out
 
+
+
+## API: DEVICE INQUIRY
+
+# p12
 # NB: 7F means all devices
 def device_inquiry(inport, outport, device_id = 0x7f):
     payload = [0x7e, device_id, 0x06, 0x01]
@@ -61,31 +75,10 @@ def device_inquiry(inport, outport, device_id = 0x7f):
         'version_dev_phase': rcv[12],
     }
 
-## NB: not working
-def db_dump(inport, outport, device_id = 0x7f):
-    payload = [0x06, 0x09, device_id, 0x06, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 ]
-    snd = mido.Message('sysex', data=payload)
-    # print(snd.hex())
-    outport.send(snd)
-    rcv = inport.receive()
-    print(rcv.hex())
-
-
-def update_master_mix(outport, v, device_id = 0x7f):
-    v_nibble = nibblize(v)
-    payload = [6,9,device_id,1,1,0,0,0,v_nibble[0],v_nibble[1],3,0,0,0,0,0,0,0,3,1,0,0,12,0,0,0]
-    snd = mido.Message('sysex', data=payload)
-    outport.send(snd)
-
-def update_master_level(outport, v, device_id = 0x7f):
-    v_nibble = nibblize(v)
-    payload = [6,9,device_id,1,1,0,0,0,v_nibble[0],v_nibble[1],3,0,0,0,0,0,0,0,3,1,0,0,13,0,0,0]
-    snd = mido.Message('sysex', data=payload)
-    outport.send(snd)
-
 
 
 ## API - REQUEST
+## generic interface defined p21
 
 def request_by_control_levels(inport, outport, request_class, control_levels, device_id = 0x7f):
     rq_class_nibble = nibblize(request_class)
@@ -97,12 +90,13 @@ def request_by_control_levels(inport, outport, request_class, control_levels, de
         # header
         6,9,device_id,6,
         # request class:
-        rq_class_nibble[0],rq_class_nibble[1],]
+        rq_class_nibble[0], rq_class_nibble[1],
+    ]
 
     # nb of control levels (A-D = 4)
-    if nb_control_levels:
-        for i in range(0,4):
-            payload.append(nb_cl_nibble[i])
+    # if nb_control_levels:
+    for i in range(0,4):
+        payload.append(nb_cl_nibble[i])
 
     for cl in control_levels:
         cl_nibble = nibblize(cl, 4)
@@ -112,7 +106,7 @@ def request_by_control_levels(inport, outport, request_class, control_levels, de
     snd = mido.Message('sysex', data=payload)
     outport.send(snd)
     rcv = inport.receive()
-    print(rcv.hex())
+    # print(rcv.hex())
     return rcv
 
 def request_by_param_type(inport, outport, request_class, param_type, device_id = 0x7f):
@@ -128,12 +122,53 @@ def request_by_param_type(inport, outport, request_class, param_type, device_id 
     ]
 
     snd = mido.Message('sysex', data=payload)
+    # print(snd.hex())
     outport.send(snd)
     rcv = inport.receive()
-    print(rcv.hex())
+    # print(rcv.hex())
     return rcv
 
+def request_by_effect(inport, outport, request_class, effect_type, effect_number, device_id = 0x7f):
+    rq_class_nibble = nibblize(request_class)
+    e_type_nibble = nibblize(effect_type)
+    e_nb_nibble = nibblize(effect_number)
+    payload = [
+        # header
+        6,9,device_id,6,
+        # request class:
+        rq_class_nibble[0], rq_class_nibble[1],
+        # effect id / number
+        e_type_nibble[0], e_type_nibble[1],
+        e_nb_nibble[0], e_nb_nibble[1],
+        # padding
+        0, 0
+    ]
+    snd = mido.Message('sysex', data=payload)
+    outport.send(snd)
+    rcv = inport.receive()
+    # print(rcv.hex())
+    return rcv
 
+def request_by_program(inport, outport, request_class, program_number, device_id = 0x7f):
+    rq_class_nibble = nibblize(request_class)
+    p_nb_nibble = nibblize(program_number)
+    payload = [
+        # header
+        6,9,device_id,6,
+        # request class:
+        rq_class_nibble[0], rq_class_nibble[1],
+        # program number
+        p_nb_nibble[0], p_nb_nibble[1],
+        # padding
+        0, 0
+    ]
+    snd = mido.Message('sysex', data=payload)
+    outport.send(snd)
+    rcv = inport.receive()
+    # print(rcv.hex())
+    return rcv
+
+# p13
 def get_system_config(inport, outport, device_id = 0x7f):
     rcv = request_by_control_levels(inport, outport, 0, [], device_id)
     rcv = rcv.bytes()
@@ -147,9 +182,9 @@ def get_system_config(inport, outport, device_id = 0x7f):
         'max_control_levels': unnibblize(rcv[55:59]),
     }
 
+## p14
 def get_param_data(inport, outport, control_levels, value_type = 'int', device_id = 0x7f):
     rcv = request_by_control_levels(inport, outport, 1, control_levels, device_id)
-    # print(rcv.hex())
     rcv = rcv.bytes()
     size_bytes = unnibblize(rcv[5:9])
     nb_control_levels = unnibblize(rcv[(9+2*size_bytes):(9+2*size_bytes)+4])
@@ -173,40 +208,168 @@ def get_param_data(inport, outport, control_levels, value_type = 'int', device_i
         'control_levels': control_levels,
     }
 
+## p16
 def get_param_display(inport, outport, control_levels, device_id = 0x7f):
     rcv = request_by_control_levels(inport, outport, 2, control_levels, device_id)
     rcv = rcv.bytes()
     size_bytes = unnibblize(rcv[5:9])
     return unnibblize_str(rcv[9:(9+2*size_bytes)])
 
+## p17
 def get_param_type(inport, outport, control_levels, device_id = 0x7f):
     rcv = request_by_control_levels(inport, outport, 3, control_levels, device_id)
     rcv = rcv.bytes()
     return unnibblize(rcv[5:9])
 
+## p18
 def get_param_desc(inport, outport, param_type, device_id = 0x7f):
     rcv = request_by_param_type(inport, outport, 4, param_type, device_id)
     rcv = rcv.bytes()
-    size_bytes = unnibblize(rcv[9:11])
+
+    p_type = unnibblize(rcv[5:9])
+
+    size_chars_label = unnibblize(rcv[9:11])
+    label = unnibblize_str(rcv[11:(11+2*size_chars_label)])
+    p = (11+2*size_chars_label)
+
+    bs=2
+    size_bytes = unnibblize(rcv[p:p+(2*bs)])
+    p += (2*bs)
+
+    bs=1
+    control_flags = unnibblize(rcv[p:p+(2*bs)])
+    p += (2*bs)
+
+    bs=2
+    option_p_type = unnibblize(rcv[p:p+(2*bs)])
+    p += (2*bs)
+
+    bs=1
+    nb_units_per_limit = unnibblize(rcv[p:p+(2*bs)])
+    p += (2*bs)
+
+    rcv = rcv[p:]
+    vals = []
+    nb_vals = math.floor(len(rcv)/(3*2*2)) # NB: this is hackish
+    for i in range(0, nb_vals):
+        vals.append({
+            'min': unnibblize(rcv[i*(3*2*2):i*(3*2*2)+4]),
+            'max': unnibblize(rcv[i*(3*2*2)+4:i*(3*2*2)+8]),
+            'display_unit': unnibblize(rcv[i*(3*2*2)+8:i*(3*2*2)+12]),
+        })
+
     return {
-        'param_type': hex(unnibblize(rcv[5:9])),
-        'label': unnibblize_str(rcv[11:(11+2*size_bytes)]),
+        'param_type': p_type,
+        'label': label,
+        'param_size': size_bytes,
+        'control_flags': control_flags,
+        'option_param_type': option_p_type,
+        'nb_units_per_limit': nb_units_per_limit,
+        'vals': vals,
     }
 
-## SAme as `get_param_desc` but returns only the label
+## Same as `get_param_desc` but returns only the label
+## p20
 def get_param_label(inport, outport, control_levels, device_id = 0x7f):
     rcv = request_by_control_levels(inport, outport, 5, control_levels, device_id)
     rcv = rcv.bytes()
-    size_bytes = unnibblize(rcv[5:9])
+    size_chars = unnibblize(rcv[5:9])
     return {
-        'label': unnibblize_str(rcv[9:(9+2*size_bytes)]),
+        'label': unnibblize_str(rcv[9:(9+2*size_chars)]),
     }
 
+# p24
+# List all parameter types (aka param numbers) for a given effect
+def get_effect_params(inport, outport, effect_type, effect_number, device_id = 0x7f):
+    rcv = request_by_effect(inport, outport, 0x18, effect_type, effect_number, device_id)
+    rcv = rcv.bytes()
+
+    label = unnibblize_str(rcv[9:31])
+    nb_params = unnibblize(rcv[31:33])
+    rcv = rcv[33:]
+    params = []
+    for i in range(0, nb_params):
+        params.append(unnibblize(rcv[i*4:i*4+4]))
+    return {
+        'label': label,
+        'params': params,
+    }
+
+# p36
+def get_program_info(inport, outport, program_number, device_id = 0x7f):
+    rcv = request_by_program(inport, outport, 0x1a, program_number, device_id)
+    rcv = rcv.bytes()
+
+    return {
+        'program_number': unnibblize(rcv[5:9]),
+        'label': unnibblize_str(rcv[9:33]),
+        'pitch_effect_nb': unnibblize(rcv[33:35]),
+        'chorus_effect_nb': unnibblize(rcv[35:37]),
+        'eq_effect_nb': unnibblize(rcv[37:39]),
+        'mod_effect_nb': unnibblize(rcv[39:41]),
+        'reverb_effect_nb': unnibblize(rcv[41:43]),
+        'delay_effect_nb': unnibblize(rcv[43:45]),
+    }
+
+
 
+## DUMPS
+
+## page 23
+def db_dump(inport, outport, device_id = 0x7f):
+    payload = [0x06, 0x09, device_id, 0x06, 0x06, 0x01,
+               0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+    snd = mido.Message('sysex', data=payload)
+    # print(snd.hex())
+    outport.send(snd)
+    rcv = inport.receive()
+    # print(rcv.hex())
+    rcv = rcv.bytes()
+    rcv_payload = rcv[5:]
+    out = []
+    nb_programs = math.floor(len(rcv_payload)/6) # NB: this is hackish
+    for i in range(0, nb_programs):
+        out.append({
+            'effect_type': unnibblize(rcv_payload[(i*(4+2)):i*(4+2)+4]),
+            'source_type': unnibblize(rcv_payload[i*(4+2)+4:i*(4+2)+6]),
+        })
+    return out
 
 def display_dump(inport, outport, device_id = 0x7f):
-    return get_param_data(inport, outport, [1, 8, 1], 'str', device_id=device_id)
+    return get_param_data(inport, outport, [1, 8, 1], 'str', device_id)
 
+
+
+## BUILD CONTROL TREE
+
+# p5
+def make_control_tree(inport, outport, device_id = 0x7f, control_level=[]):
+
+    # print("------------------------")
+    # pprint(control_level)
+
+    # tree={}
+
+    p_type = get_param_type(inport, outport, control_level, device_id)
+    p_desc = get_param_desc(inport, outport, p_type, device_id)
+    p = {
+        'type': p_type,
+        'desc': p_desc,
+    }
+
+    if p_desc['control_flags'] == 0x04: # non-editable param, i.e. tree branching
+        p['children'] = {}
+        for v in p_desc['vals']:
+            for cl_id in range(v['min'], v['max']+1):
+                cl = control_level.copy()
+                cl.append(cl_id)
+                branch = make_control_tree(inport, outport, device_id, cl)
+                p['children'][cl_id] = branch
+    return p
+
+
+
+## SPECIFIC ACCESS
 
 def get_param_eq_m_gain(inport, outport, device_id = 0x7f):
     control_levels = [
@@ -219,12 +382,24 @@ def get_param_eq_m_gain(inport, outport, device_id = 0x7f):
         # level D - Param: Gain
         2
     ]
-    return get_param_data(inport, outport, control_levels, device_id=device_id)
+    return get_param_data(inport, outport, control_levels, device_id)
+
+def update_master_mix(outport, v, device_id = 0x7f):
+    v_nibble = nibblize(v)
+    payload = [6,9,device_id,1,1,0,0,0,v_nibble[0],v_nibble[1],3,0,0,0,0,0,0,0,3,1,0,0,12,0,0,0]
+    snd = mido.Message('sysex', data=payload)
+    outport.send(snd)
+
+def update_master_level(outport, v, device_id = 0x7f):
+    v_nibble = nibblize(v)
+    payload = [6,9,device_id,1,1,0,0,0,v_nibble[0],v_nibble[1],3,0,0,0,0,0,0,0,3,1,0,0,13,0,0,0]
+    snd = mido.Message('sysex', data=payload)
+    outport.send(snd)
 
 
 
 
-## MAIN
+## TESTS
 
 # resp = device_inquiry(inport, outport, 0x00)
 # resp = db_dump(inport, outport, 0x00)
@@ -237,14 +412,66 @@ def get_param_eq_m_gain(inport, outport, device_id = 0x7f):
 
 
 # resp = get_param_display(inport, outport, [0, 2, 1, 2], 0x00)
-p_type = get_param_type(inport, outport, [0, 2, 1, 2], 0x00)
-resp = get_param_desc(inport, outport, p_type, 0x00)
+# p_type = get_param_type(inport, outport, [0, 2, 1, 2], 0x00)
+# resp = get_param_desc(inport, outport, p_type, 0x00)
 # resp = get_param_label(inport, outport, [0, 2, 1, 2], 0x00)
 
 # resp = get_system_config(inport, outport, 0x00)
 
-pprint(resp)
+# p_type = get_param_type(inport, outport, [0, 2, 1, 2], 0x00)
+# print(p_type)
+# resp = get_param_desc(inport, outport, p_type, 0x00)
 
+# resp = request_by_control_levels(inport, outport, 0, [1], 0x00)
+
+# resp = get_effect_params(inport, outport, 1, 1, 0x00)
+
+# resp = get_program_info(inport, outport, 0, 0x00)
+
+# resp = get_param_data(inport, outport, [0], device_id = 0x00)
+
+# pprint(resp)
+
+
+
+## MAIN
+
+device_id = 0x00
+
+# 1- get number of parameters
+# sysconf = get_system_config(inport, outport, device_id)
+# nb_params = sysconf['nb_params']
+
+# 2- build control tree
+
+# 2-1- db of all param desc by param type (aka param number)
+# NB: this is slow AF
+# p_type_desc_map={}
+# for p_type in range(0, nb_params):
+#     p_desc = get_param_desc(inport, outport, p_type, device_id)
+#     p_type_desc_map[p_type] = p_desc
+
+# 2-1- actual control tree
+# root_p_type = get_param_type(inport, outport, [], device_id)
+# root_p_desc = p_type_desc_map[root_p_type]
+# root_p_desc = get_param_desc(inport, outport, 1, device_id)
+
+# pprint(root_p_type)
+
+# TODO: test that is not editable param -> tree branching
+# for cl_id in range(root_p_desc['vals'][0]['min'], root_p_desc['vals'][0]['max']+1):
+#     p_type = get_param_type(inport, outport, [cl_id], device_id)
+#     p_desc = get_param_desc(inport, outport, p_type, device_id)
+#     print("------------------")
+#     pprint(p_desc)
+
+
+# NB: this is slow AF
+# control_tree = make_control_tree(inport, outport, device_id)
+# pprint(control_tree)
+
+
+# pprint(root_p_desc)
 
 
 ## CLEANUP
